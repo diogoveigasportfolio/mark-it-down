ee7c034c0ecda1bbc32b50792d8c4d401547ee24
Diogo Veigas <diogoveigas@proton.me>
2024-04-20 14:46:22 +0100

    Update styles on file path

--- src/renderer/src/pages/Home/components/FilePath.tsx
+++ src/renderer/src/pages/Home/components/FilePath.tsx
@@ -8,7 +8,9 @@ type FilePathProps = {
 }
 
 export function FilePath({ anySelected, selectedItem }: FilePathProps) {
-  if (!anySelected) return <p>Click in a file to open it...</p>
+  const colorClasses = 'text-neutral-900 dark:text-neutral-100'
+
+  if (!anySelected) return <p className={colorClasses}>Click in a file to open it...</p>
 
   const path: string[] = []
 
@@ -22,10 +24,8 @@ export function FilePath({ anySelected, selectedItem }: FilePathProps) {
     <>
       {path.map((item, index) => (
         <span className="flex items-center" key={item}>
-          {index !== 0 && (
-            <HiMiniChevronRight className="size-6 stroke-2 text-neutral-900 dark:text-neutral-100" />
-          )}
-          <span className="text-lg text-neutral-900 dark:text-neutral-100">{item}</span>
+          {index !== 0 && <HiMiniChevronRight className={`size-6 stroke-2 ${colorClasses}`} />}
+          <span className={`text-lg ${colorClasses}`}>{item}</span>
         </span>
       ))}
     </>
