1d9ab8da8154e6450a75ca3cb4ffcc62e3529157
Diogo Veigas <diogoveigas@proton.me>
2024-04-21 13:37:32 +0100

    Readjust padding on markdown based on screen size

--- src/renderer/src/pages/Home/components/MarkdownEditor.tsx
+++ src/renderer/src/pages/Home/components/MarkdownEditor.tsx
@@ -62,7 +62,7 @@ export function MarkdownEditor({ selectedItem, setItems }: MarkdownEditorProps) 
           markdownShortcutPlugin()
         ]}
         autoFocus={{ defaultSelection: 'rootEnd' }}
-        contentEditableClassName="overflow-auto outline-none h-screen px-12 sm:px-24 caret-blue-500 prose prose-neutral dark:prose-invert prose-lg prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
+        contentEditableClassName="overflow-auto outline-none h-screen px-12 lg:px-24 caret-blue-500 prose prose-neutral dark:prose-invert prose-lg prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
       />
     </div>
   )
