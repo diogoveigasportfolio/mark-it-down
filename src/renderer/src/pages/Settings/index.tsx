646ca78c3de558122796a8a560029629c02f3c75
Diogo Veigas <diogoveigas@proton.me>
2024-05-05 10:48:17 +0100

    settings page top bar

--- src/renderer/src/pages/Settings/index.tsx
+++ src/renderer/src/pages/Settings/index.tsx
@@ -1,10 +1,18 @@
+import { HiOutlineXCircle } from 'react-icons/hi2'
 import { Link } from 'react-router-dom'
 
 function Settings() {
   return (
     <>
-      <div>Settings</div>
-      <Link to="/">Go to Home</Link>
+      <div className="bg-neutral-150 dark:bg-neutral-800 w-auto min-h-16 flex items-center gap-3 px-4">
+        <Link to="/">
+          <HiOutlineXCircle
+            className="text-neutral-550 dark:text-neutral-450 hover:text-neutral-750 hover:dark:text-neutral-250"
+            size={40}
+          />
+        </Link>
+        <h1 className="text-xl text-neutral-800 dark:text-neutral-150">Settings</h1>
+      </div>
     </>
   )
 }
