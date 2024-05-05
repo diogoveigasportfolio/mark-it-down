487ae3a28d42d89aa8ac8de10852900295026686
Diogo Veigas <diogoveigas@proton.me>
2024-05-05 11:34:28 +0100

    Sidebar component

--- src/renderer/src/pages/Settings/components/Header.tsx
+++ src/renderer/src/pages/Settings/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export function Header() {
   return (
-    <div className="bg-neutral-150 dark:bg-neutral-800 w-auto min-h-16 flex items-center gap-3 px-4">
+    <header className="bg-neutral-150 dark:bg-neutral-800 w-auto min-h-16 flex items-center gap-3 px-4">
       <Link to="/">
         <HiOutlineXCircle
           className="text-neutral-550 dark:text-neutral-450 hover:text-neutral-750 hover:dark:text-neutral-250"
@@ -11,6 +11,6 @@ export function Header() {
         />
       </Link>
       <h1 className="text-xl text-neutral-800 dark:text-neutral-150">Settings</h1>
-    </div>
+    </header>
   )
 }
--- src/renderer/src/pages/Settings/index.tsx
+++ src/renderer/src/pages/Settings/index.tsx
@@ -1,77 +1,27 @@
 import useLocalStorage from '@renderer/hooks/useLocalStorage'
+import useSideBarSizes from '@renderer/hooks/useSideBarSizes'
 import { explorerData } from '@renderer/data/explorerData'
 import { ExplorerItemType, FileType } from '@renderer/typings'
 
-import { Header } from './components'
-import SelectableItem from '@renderer/components/ExplorerItem/SelectableItem'
+import { Header, Sidebar } from './components'
+import { SplitManager } from '../Home/components'
 
 function Settings() {
+  const { sideBarSizes, sideBarIsOpen, onDragEnd } = useSideBarSizes()
   const { value: items, setValue: setItems } = useLocalStorage<ExplorerItemType[]>('notes', [
     ...explorerData
   ])
 
-  function handleToggleFolder(id: string) {
-    setItems((prevItems) => {
-      return prevItems.map((item) => {
-        if ('children' in item && item.id === id) {
-          return {
-            ...item,
-            isOpen: !item.isOpen
-          }
-        }
-        return item
-      })
-    })
-  }
-
-  function handleToggleSelect(id: string, override?: boolean) {
-    setItems((prevItems) => {
-      return prevItems.map((folder) => {
-        if (!('children' in folder)) return folder
-
-        if (folder.id === id) {
-          return {
-            ...folder,
-            isSelected: override ? override : !folder.isSelected,
-            children: childrenSelection(folder.children)
-          }
-        }
-        return {
-          ...folder,
-          isSelected: false,
-          children: childrenSelection(folder.children)
-        }
-      })
-    })
-
-    function childrenSelection(files: FileType[]) {
-      return files.map((file) => {
-        if (file.id === id) {
-          return {
-            ...file,
-            isSelected: override ? override : !file.isSelected
-          }
-        }
-        return {
-          ...file,
-          isSelected: false
-        }
-      })
-    }
-  }
-
   return (
     <>
       <Header />
-      {items.map((item) => (
-        <SelectableItem
-          key={item.id}
-          item={item}
-          handleToggleFolder={handleToggleFolder}
-          handleToggleSelect={handleToggleSelect}
-          selectableChildren={true}
-        />
-      ))}
+      <SplitManager onDragEnd={onDragEnd} sideBarSizes={sideBarSizes} sideBarIsOpen={sideBarIsOpen}>
+        <Sidebar items={items} setItems={setItems} />
+        <section className="h-full bg-neutral-100 dark:bg-neutral-850 w-auto flex flex-col overflow-hidden">
+          Content will be here...
+        </section>
+      </SplitManager>
+      
     </>
   )
 }
