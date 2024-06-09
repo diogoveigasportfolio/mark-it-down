7abab93f9cd9b874a5ad4e0bace55f9aa9a87423
Diogo Veigas <diogoveigas@proton.me>
2024-06-09 13:27:24 +0100

    Espace keyword closes settings and goes back home.

--- src/renderer/src/pages/Settings/index.tsx
+++ src/renderer/src/pages/Settings/index.tsx
@@ -1,13 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+
 import useLocalStorage from '@renderer/hooks/useLocalStorage'
 import useSideBarSizes from '@renderer/hooks/useSideBarSizes'
 import { ExplorerItemType } from '@renderer/typings'
 
 import { Footer, Header, Sidebar, Content } from './components'
 import SplitManager from '@renderer/components/SplitManager'
+import useKeydown from '@renderer/hooks/useKeydown'
 
 function Settings() {
   const { sideBarSizes, sideBarIsOpen, onDragEnd } = useSideBarSizes()
   const { value: items, setValue: setItems } = useLocalStorage<ExplorerItemType[]>('notes')
+
+  const navigate = useNavigate()
+
+  // * Navigate to the home page
+  useKeydown('Escape', () => {
+    navigate(-1)
+  })
 
   return (
     <div className="h-full w-full overflow-y-hidden">
